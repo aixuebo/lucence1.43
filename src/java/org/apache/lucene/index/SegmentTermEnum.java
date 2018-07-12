46f883ec73308161ba609a787659439348129b59
aixuebo <78608544@qq.com>
2018-07-12 15:26:43 +0800

    Update SegmentTermEnum.java

--- src/java/org/apache/lucene/index/SegmentTermEnum.java
+++ src/java/org/apache/lucene/index/SegmentTermEnum.java
@@ -14,6 +14,7 @@ package org.apache.lucene.index;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 解析tis或者tii文件---解析每一个term内容
  */
 
 import java.io.IOException;
@@ -22,29 +23,32 @@ import org.apache.lucene.store.InputStream;
 final class SegmentTermEnum extends TermEnum implements Cloneable {
   private InputStream input;
   FieldInfos fieldInfos;
-  long size;
-  long position = -1;
+  long size;//总term数量
+  long position = -1;//当前迭代到哪个term上了
 
-  private Term term = new Term("", "");
-  private TermInfo termInfo = new TermInfo();
+  private Term term = new Term("", "");//该词的内容
+  private TermInfo termInfo = new TermInfo();//该词的统计维度信息
 
   private int format;
-  private boolean isIndex = false;
-  long indexPointer = 0;
+  private boolean isIndex = false;//是否是索引文件
+  long indexPointer = 0;//索引tii文件要获取tis的文件位置
   int indexInterval;
   int skipInterval;
+  
   private int formatM1SkipInterval;
-  Term prev;
+  
+  Term prev;//前一个term词
 
   private char[] buffer = {};
 
+  //初始化
   SegmentTermEnum(InputStream i, FieldInfos fis, boolean isi)
           throws IOException {
     input = i;
     fieldInfos = fis;
-    isIndex = isi;
+    isIndex = isi;//是否是tii索引文件
 
-    int firstInt = input.readInt();
+    int firstInt = input.readInt();//版本号version
     if (firstInt >= 0) {
       // original-format file, without explicit format version number
       format = 0;
@@ -104,19 +108,21 @@ final class SegmentTermEnum extends TermEnum implements Cloneable {
     growBuffer(term.text.length());		  // copy term text into buffer
   }
 
-  /** Increments the enumeration to the next element.  True if one exists.*/
+  /** Increments the enumeration to the next element.  True if one exists
+   * 迭代每一个term词
+  */
   public final boolean next() throws IOException {
-    if (position++ >= size - 1) {
+    if (position++ >= size - 1) {//说明已经没有term可以被迭代了
       term = null;
       return false;
     }
 
-    prev = term;
-    term = readTerm();
+    prev = term;//获取前一个term
+    term = readTerm();//获取当前term
 
-    termInfo.docFreq = input.readVInt();	  // read doc freq
-    termInfo.freqPointer += input.readVLong();	  // read freq pointer
-    termInfo.proxPointer += input.readVLong();	  // read prox pointer
+    termInfo.docFreq = input.readVInt();	  // read doc freq 该term出现在多少个document中
+    termInfo.freqPointer += input.readVLong();	  // read freq pointer 该term词频文件的位置
+    termInfo.proxPointer += input.readVLong();	  // read prox pointer  该tem词位置文件的位置
     
     if(format == -1){
     //  just read skipOffset in order to increment  file pointer; 
@@ -129,25 +135,26 @@ final class SegmentTermEnum extends TermEnum implements Cloneable {
     }
     else{
       if (termInfo.docFreq >= skipInterval) 
-        termInfo.skipOffset = input.readVInt();
+        termInfo.skipOffset = input.readVInt();//该值暂时没看到实际用的地方,因此暂时不了解
     }
     
     if (isIndex)
-      indexPointer += input.readVLong();	  // read index pointer
+      indexPointer += input.readVLong();	  // read index pointer  索引tii文件要获取tis的文件位置
 
     return true;
   }
 
+  //读取下一个term的具体内容
   private final Term readTerm() throws IOException {
     int start = input.readVInt();
     int length = input.readVInt();
-    int totalLength = start + length;
+    int totalLength = start + length;//该term总字节
     if (buffer.length < totalLength)
       growBuffer(totalLength);
 
-    input.readChars(buffer, start, length);
+    input.readChars(buffer, start, length);//读取term剩余字节到buffer中
     return new Term(fieldInfos.fieldName(input.readVInt()),
-            new String(buffer, 0, totalLength), false);
+            new String(buffer, 0, totalLength), false);//获取该term的field以及词内容
   }
 
   private final void growBuffer(int length) {
