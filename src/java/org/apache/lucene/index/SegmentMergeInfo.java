2abbb115eacf8000aa6870a3869ea73fef26b5f4
aixuebo <78608544@qq.com>
2018-07-12 19:23:01 +0800

    Update SegmentMergeInfo.java

--- src/java/org/apache/lucene/index/SegmentMergeInfo.java
+++ src/java/org/apache/lucene/index/SegmentMergeInfo.java
@@ -19,35 +19,41 @@ package org.apache.lucene.index;
 import java.io.IOException;
 
 final class SegmentMergeInfo {
-  Term term;
-  int base;
-  TermEnum termEnum;
-  IndexReader reader;
-  TermPositions postings;
-  int[] docMap = null;				  // maps around deleted docs
+  Term term;//最上面的term
+  int base;//该segment的docId的第一个序号,保证所有segment的序号都不重复,并且连贯
+  TermEnum termEnum;//该segment的term迭代器
+  IndexReader reader;//segment的索引reader对象
+  TermPositions postings;//SegmentTermPositions对象
+  int[] docMap = null;				  // maps around deleted docs  每一个doc文档的唯一序号,真实的序号是base+该文档数组对应的数字
 
+  /**
+  * 参数b是docid的基础序号
+  * 参数 te是segment上的term迭代器集合
+  * r 表示索引的reader对象
+  **/
   SegmentMergeInfo(int b, TermEnum te, IndexReader r)
     throws IOException {
     base = b;
     reader = r;
     termEnum = te;
-    term = te.term();
+    term = te.term();//最上面的第一个词
     postings = reader.termPositions();
 
     // build array which maps document numbers around deletions 
-    if (reader.hasDeletions()) {
+    if (reader.hasDeletions()) {//如果有删除的docid
       int maxDoc = reader.maxDoc();
       docMap = new int[maxDoc];
-      int j = 0;
+      int j = 0;//新的id序号   真实的id是base+j
       for (int i = 0; i < maxDoc; i++) {
         if (reader.isDeleted(i))
-          docMap[i] = -1;
+          docMap[i] = -1;//因为该文档被删除了,因此使用-1代替他的序号，即他不需要序号了
         else
-          docMap[i] = j++;
+          docMap[i] = j++; //让他的id连贯起来
       }
     }
   }
 
+  //不断的获取下一个term
   final boolean next() throws IOException {
     if (termEnum.next()) {
       term = termEnum.term();
